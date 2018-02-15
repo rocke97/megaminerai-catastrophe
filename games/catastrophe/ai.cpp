60f95c0a291b48fcdb3e0ebbbd847181740d61d3
rocke97 <aarongunther97@gmail.com>
2018-02-15 08:07:28 -0600

    Testing remote set up and AI name change

--- games/catastrophe/ai.cpp
+++ games/catastrophe/ai.cpp
@@ -26,7 +26,7 @@ std::string AI::get_name() const
 {
     // <<-- Creer-Merge: get-name -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
     // REPLACE WITH YOUR TEAM NAME!
-    return "Arrays Start at Zero";
+    return "Simple Minded AI";
     // <<-- /Creer-Merge: get-name -->>
 }
 
